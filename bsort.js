a4578969b6abdfd4298a9a67264ab7868097ea54
UnrealSec <arran.bishop89@aol.com>
2020-09-10 17:11:54 +0300

    Update bsort.js

--- bsort.js
+++ bsort.js
@@ -1,9 +1,9 @@
 function bsort(array, comparer) {
-    var flag = true;
+    let flag = true;
     while (flag) {
         flag = false;
         for (let i=0; i<array.length-1; i++) {
-            var temp = array[i+1];
+            let temp = array[i+1];
             if (comparer(array[i], temp) === true) {
                 flag = true;
                 array[i+1] = array[i];
